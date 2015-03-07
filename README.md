cce03e8d667a25ebdbdebe9b5a66fecebcf3feec
regpar <admin@gizmogrand.com>
2015-03-07 04:53:38 -0500

    new file:   LICENSE new file:   README.md new file:   gerpw.sh

---
+++ README.md
@@ -0,0 +1,146 @@
+# gerpw
+bash script GnuPG password manager
+
+		gerpw
+		Generate, Edit, Read Passwords
+		by regpar
+
+
+A simple script written in bash, making it simple
+to use GnuPG as your a password manager. The folder location
+and heirarchies of your encrypted password files are 
+independent of the script.
+
+
+Options:
+
+	-f  	define filename			
+	-i 		define GnuPG userid
+	-u  	define username
+	-p  	define password
+
+	-g		generate password
+	-n		new entry
+	-e		edit entry		
+	-r		read entry
+
+	-x		copy <username>
+	-c		copy <passcode>
+
+Dependencies:
+
+	Bash		http://www.gnu.org/software/bash/
+	GnuPG		https://www.gnupg.org/
+	Mktemp		http://www.mktemp.org/
+	
+	Optional:
+	xclip		http://sourceforge.net/projects/xclip/
+
+Basic Syntax:
+
+	$ sh gerpw.sh -f <filename> -i <gpg userid> -g -n -r
+
+		-g	generate password using /dev/urandom
+		-n	create a new entry
+		-r	read an encrypted entry
+
+-----or-----
+
+	$ sh gerpw.sh -f <filename> -i <gpg userid> -u <username> -p <password> -g -e -r
+
+		-g	generate password using /dev/urandom
+		-e	edit an existing entry
+		-r	rean an encrypted entry
+	
+
+Examples:
+
+	$ sh gerpw.sh -f example.com -i testing@email.com -g -n -r
+	
+		generating password:
+		enter password length
+		20
+		
+		Generated Password: hO0rFryXPg2RFtjxvG8p
+		
+		enter username:
+		USERNAME
+		
+		
+		###########[---example.com---]###########
+
+		USERNAME
+		hO0rFryXPg2RFtjxvG8p
+
+		###########[---example.com---]###########
+
+----------------
+
+	$ sh gerpw.sh -f example.com -i testing@email.com -g -e -r
+		
+		generating password:
+		enter password length
+		20
+		
+		Generated Password: IMwZSJ7dlzAoPN27GAVk
+		
+		Current username: USERNAME
+		Do you want to change it? (yes/no)
+		yes
+		
+		Enter NEW username:
+		NEW_USERNAME
+
+		###########[---example.com---]###########
+		
+		NEW_USERNAME
+		IMwZSJ7dlzAoPN27GAVk
+		
+		###########[---example.com---]###########
+
+Advanced Examples:
+
+	$ sh gerpw.sh -f example.com -i testing@email.com -u USERNAME22 -p PASSWORD44 -nr
+
+	     	
+		###########[---example.com---]###########
+
+		USERNAME22
+		PASSWORD44
+
+		###########[---example.com---]###########
+
+----------------
+
+	$ sh gerpw.sh -f example.com -i testing@email.com -u USERNAME -ger
+
+		generating password:
+		enter password length
+		30
+
+		Generated Password: fBTvpJ0GZm4SSwSxhHcxCtoty7wlLq
+
+		editting gerpw entry: example.com
+
+
+		###########[---example.com---]###########
+
+		USERNAME
+		fBTvpJ0GZm4SSwSxhHcxCtoty7wlLq
+
+		###########[---example.com---]###########
+
+
+Optional Copy w/ xclip:
+
+	# Copy <username> to clipboard
+	$ sh gerpw.sh -f example.com -i testing@email.com -x
+
+	# Copy <password> to clipboard
+	$ sh gerpw.sh -f example.com -i testing@email.com -c
+
+
+User Defined Options:
+
+	[1] default reader to edit entries (example: nano, vim)
+	[2] default GnuPG UserID, making the -i <gpg userid> optional
---
+++ gerpw.sh
@@ -0,0 +1,229 @@
+#!/bin/bash/env bash
+
+## User Defined Defaults--------------------------
+
+### GnuPG UserID
+GERPuser=email@email
+
+### Default Editor
+EDITOR="${vi:-nano}"
+
+##------------------------------------------------
+
+
+usage () {
+        cat <<_
+Usage: gerp-bash.sh -f <filename> -i <gpg userid> <Parameters>
+        where <Parameters> can be any of 
+        -g      generate password
+        -e      edit a password
+	-n	enter a new password
+        -r      read an encrypted password
+	
+	-u	<username>
+	-p	<password>
+
+	-cu	copy <username>
+	-cp	copy <password>
+	
+#USE  [ $ unset HISTFILE ]  prior to using gerpw to prevent bash history from saving & close terminal window when done.
+_
+	
+}
+
+while getopts "i:f::u::p:genrcx" opt; do
+ case $opt in
+ 
+     f) gerp=$OPTARG;;
+     i) gerpuser=$OPTARG;;
+     u) tagname=$OPTARG; usercounter=1;;			
+     p) tagpass=$OPTARG; passcounter=1;;
+
+     g) genecounter=1;;
+     n) newcounter=1;;
+     e) editcounter=1;;
+     r) readcounter=1;;
+     x) xclip=user;;
+     c) xclip=pass;;
+     
+     h) usage; exit;;
+     ?) usage; exit;;
+    esac
+done
+
+## mutually exclusive
+if [ "$newcounter" = "1" ] && [ "$editcounter" = "1" ]; then
+    echo
+    echo "-n & -e are mutually exclusive options. Ending Program."
+    echo
+    exit
+elif [ "$genecounter" = "1" ] && [ "$passcounter" = "1" ]; then
+    echo
+    echo "-p & -g are mutually exclusive options. Ending Program."
+    echo
+    exit
+fi
+
+function on_exit {
+    rm -rf "$gerptemp" "$gerptemp2"
+}
+
+## -g --generate tag
+
+if [ "$genecounter" = "1" ]; then
+echo "generating password:"
+    echo "enter password length"
+    read pwlength
+    genpw=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w $pwlength | head -n 1);
+    echo
+    echo "Generated Password: $genpw"
+    echo				
+fi
+
+## -n --new_entry tag
+
+if [ "$newcounter" = "1" ]; then
+    gerptemp="($mktemp)"
+    trap on_exit EXIT
+    if [ "$usercounter" = "1" ]; then
+	username=$tagname
+	
+    else	
+	echo "enter username:"
+	read "username"
+	echo
+
+    fi;
+
+{    
+    if [ "$genecounter" = "1" ]; then
+	(echo "$username" && echo "$genpw") > "$gerptemp"
+	gpg -o $gerp -r $gerpuser --encrypt $gerptemp &> /dev/null
+
+    else
+
+	    if [ "$passcounter" = "1" ]; then
+		inputpw=$tagpass
+		(echo "$username" && echo "$inputpw") > "$gerptemp"
+		gpg -o $gerp -r $gerpuser --encrypt $gerptemp &> /dev/null
+
+	    else
+		echo "enter password:"
+		read inputpw
+		(echo "$username" && echo "$inputpw") > "$gerptemp"
+		gpg -o $gerp -r $gerpuser --encrypt $gerptemp &> /dev/null
+
+	    fi
+    fi
+}
+fi
+
+## -e --edit_entry tag
+
+if [ "$editcounter" = "1" ]; then
+     echo "editting garp-bash entry: $GERP"
+     gerptemp="$(mktemp)"
+     trap on_exit EXIT
+     gpg -o $gerptemp --batch --yes  --decrypt $gerp &> /dev/null
+     
+     if [ "$genecounter" = "1" ]; then
+	 echo
+	 username="$(sed -n '1p' $gerptemp)"
+
+	 {
+	     if [ "$usercounter" = "1" ]; then
+		(echo "$tagname" && echo "$genpw") > $gerptemp
+		gpg -o $gerp -r $gerpuser --batch --yes  --encrypt $gerptemp &> /dev/null
+
+	     else
+		 echo "Current username: $username"
+		 echo "Do you want to change it? (yes/no)"
+		 read uduncounter
+
+		 {
+		     if [ "$uduncounter" = "yes" ] || [ "$uduncounter" = "y" ]; then
+		         echo
+			 echo "Enter NEW username:"
+			 read inputun
+			 (echo "$inputun" && echo "$genpw") >| "$gerptemp"
+			 gpg -o $gerp -r $gerpuser --batch --yes --encrypt $gerptemp &> /dev/null
+
+		     elif [ "$uduncounter" = "no" ] || [ "$uduncounter" = "n" ]; then
+			 (echo "$username" && echo "$genpw") >| $gerptemp
+			 gpg -o $gerp -r $gerpuser --batch --yes --encrypt $gerptemp &> /dev/null
+
+		     else
+			 echo
+			 echo "Invalid Option. Ending Program"
+			exit
+			 
+		     fi
+		 }
+	     fi
+	 }
+
+
+     else
+	 {
+	     if [ "$usercounter" = "1" ]; then
+		 {
+		     if [ "$passcounter" = "1" ]; then
+			 (echo "$tagname" && echo "$tagpass") >| $gerptemp
+			 gpg -o $gerp -r $gerpuser --batch --yes --encrypt $gerptemp &> /dev/null
+			
+		     else
+			 uneditpw="$(sed -n '2p' $gerptemp)"
+			 (echo "$tagname" && echo "$uneditpw") >| $gerptemp
+			 gpg -o $gerp -r $gerpuser --batch --yes --encrypt $gerptemp &> /dev/null
+
+		     fi;
+		 }
+
+	     elif [ "$passcounter" = "1" ]; then
+		username="$(sed -n '1p' $gerptemp)"
+		(echo "$username" && echo "$tagpass") >| $gerptemp
+		gpg -o $gerp-r $gerpuser --batch --yes --encrypt $gerptemp &> /dev/null
+
+	     else
+		 $EDITOR $gerptemp
+		 gpg -o $gerp -r $gerpuser --batch --yes --encrypt $gerptemp &> /dev/null
+	     fi;
+	 }
+     fi
+fi
+
+## -r --read_entry tag
+
+if [ "$readcounter" = "1" ]; then
+    gerptemp2="$(mktemp)"
+    trap on_exit EXIT
+    echo
+    echo "###########[---$gerp---]###########"
+    echo
+    gpg --decrypt --batch --yes -o $gerptemp2 $gerp &> /dev/null
+    cat $gerptemp2
+    echo
+    echo "###########[---$gerp---]###########"
+    echo
+
+    if [ "$xclip" = "pass" ]; then
+	echo "$(sed -n '2p' $gerptemp2)" | xclip &> /dev/null
+    elif [ "$xclip" = "USER" ]; then
+	echo "$(sed -n '1p' $gerptemp2)" | xclip &> /dev/null
+    fi
+exit
+
+else
+    gerptemp2="$(mktemp)"
+    trap on_exit EXIT
+    if [ "$xclip" = "pass" ]; then
+	gpg --decrypt --batch --yes -o $gerptemp2 $gerp &> /dev/null
+	echo "$(sed -n '2p' $gerptemp2)" | xclip &> /dev/null
+	
+    elif [ "$xclip" = "user" ]; then
+	gpg --decrypt --batch --yes -o $gerptemp2 $gerp &> /dev/null
+	echo "$(sed -n '1p' $gerptemp2)" | xclip &> /dev/null
+    fi
+	exit
+fi
+
